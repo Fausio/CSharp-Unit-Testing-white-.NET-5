a8d85441e7a5c7b96d78700369f18d44c30db4c1
Fáusio <luisfausio@gmail.com>
2021-08-18 13:57:04 +0200

    MyClasses thst is Mstest for UT

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
