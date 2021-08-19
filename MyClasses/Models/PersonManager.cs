4384c76fbc629e7292820251b078f04ff940aec2
Fáusio <luisfausio@gmail.com>
2021-08-19 21:51:12 +0200

    PersonManager cs

---
+++ MyClasses/Models/PersonManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClasses.Models
+{
+    public class PersonManager
+    {
+        public Person CreatePerson(string fname, string lname, bool IsSupervisor)
+        {
+            Person person = null;
+
+            if (!string.IsNullOrEmpty(fname))
+            {
+                if (IsSupervisor)
+                {
+                    person = new Supervisor();
+                }
+                else
+                {
+                    person = new Employee();
+                }
+
+                // generic props
+                person.FirstName = "Fausio ";
+                person.LastName = "Luis ";
+            }
+
+            return person;
+        }
+    }
+}
