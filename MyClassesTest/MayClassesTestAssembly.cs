c4971d44a78dd61e6c871cd56aa2cbec6e4de1a2
Fáusio <luisfausio@gmail.com>
2021-08-19 13:27:35 +0200

    MayClassesTestAssembly with AssemblyInitialize and  AsseblueCleanUp

---
+++ MyClassesTest/MayClassesTestAssembly.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class MayClassesTestAssembly
+    {
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext tc)
+        {
+            tc.WriteLine("Running AssemblyInitialize");
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+             
+        }
+
+    }
+}
