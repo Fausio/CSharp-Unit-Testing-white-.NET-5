7fabe9cc94d6148dd14c0411100432458af8f178
Fáusio <luisfausio@gmail.com>
2021-08-19 22:19:08 +0200

    Supervisor

---
+++ MyClasses/Models/Supervisor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClasses.Models
+{
+    public  class Supervisor : Person
+    {
+        public bool IsSupervisor { get; set; }
+        public IEnumerable<Person> Properties()
+        { 
+            return new List<Person>() ;
+        }
+    }
+}
