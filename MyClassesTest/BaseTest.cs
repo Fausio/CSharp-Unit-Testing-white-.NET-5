999a2001a5f8c48de64ef2191c651ef1e8312c1e
Fáusio <luisfausio@gmail.com>
2021-08-19 12:13:41 +0200

    BaseTest class implementation

--- MyClassesTest/BaseTest.cs
+++ MyClassesTest/BaseTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,29 @@ namespace MyClassesTest
 {
     public class BaseTest
     {
+        protected string _GOOD_FILE_NAME;
+        protected const string BAD_FILE_NAME = @"C:\Windows\PFRO.txt";
+        protected const string GOOD_FILE_NAME = @"C:\Windows\PFRO.log";
+
+
+        private TestContext testContextInstance1;
+
+        //used to call the base methods of TestContext
+        protected TestContext TestContext
+        {
+            get { return testContextInstance1; }
+            set { testContextInstance1 = value; }
+        }
+
+
+        protected void set_BAD_FILE_NAME()
+        {
+            _GOOD_FILE_NAME = TestContext.Properties["GoodFileName"].ToString();
+
+            if (_GOOD_FILE_NAME.Contains("[AppPath]"))
+            {
+                _GOOD_FILE_NAME = _GOOD_FILE_NAME.Replace("[AppPath]", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
+        }
     }
 }
