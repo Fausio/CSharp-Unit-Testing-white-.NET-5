894ccdcac0fbf96b480c8de3368efe8b8f2995be
Fáusio <luisfausio@gmail.com>
2021-08-19 22:19:33 +0200

    IsNull

---
+++ MyClassesTest/PersonTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassesTest
+{
+    [TestClass]
+   public class PersonTest
+    {
+        [TestMethod]
+        public void IsInstanceOfTypeTest()
+        {
+            PersonManager pmg = new PersonManager();
+            Person per;
+
+            per = pmg.CreatePerson("Faizal","Isuffo", true);
+
+            Assert.IsInstanceOfType(per, typeof(Supervisor));
+        }
+
+        [TestMethod]
+        public void IsNull()
+        {
+            PersonManager pmg = new PersonManager();
+            Person per;
+
+            per = pmg.CreatePerson("", "Isuffo", true);
+
+            Assert.IsNull(per);
+        }
+    }
+}
