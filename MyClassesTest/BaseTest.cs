6464352193e2c704a7a1a481570bd9676cc75891
Fáusio <luisfausio@gmail.com>
2021-08-20 08:30:19 +0200

    LoadDataTable from db

--- MyClassesTest/BaseTest.cs
+++ MyClassesTest/BaseTest.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -13,9 +15,45 @@ namespace MyClassesTest
         protected string _GOOD_FILE_NAME;
         protected const string BAD_FILE_NAME = @"C:\Windows\PFRO.txt";
         protected const string GOOD_FILE_NAME = @"C:\Windows\PFRO.log";
-
-
+        public DataTable TestDataTable { get; set; } 
         private TestContext testContextInstance1;
+
+        
+
+
+
+        public DataTable LoadDataTable(string sql, string connection)
+        {
+            TestDataTable = null;
+
+            try
+            {
+                // Create a connection
+                using (SqlConnection ConnectionObject = new SqlConnection(connection))
+                {
+                    // Create command object
+                    using (SqlCommand CommandObject = new SqlCommand(sql, ConnectionObject))
+                    {
+                        // Create a SQL Data Adapter
+                        using (SqlDataAdapter da = new SqlDataAdapter(CommandObject))
+                        {
+                            // Fill DataTable using Data Adapter
+                            TestDataTable = new DataTable();
+                            da.Fill(TestDataTable);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                TestContext.WriteLine("Error in LoadDataTable() method." + Environment.NewLine + ex.ToString());
+            }
+
+            return TestDataTable;
+        }
+
+
+
 
         //used to call the base methods of TestContext
         public TestContext TestContext
@@ -23,7 +61,7 @@ namespace MyClassesTest
             get { return testContextInstance1; }
             set { testContextInstance1 = value; }
         }
-
+       
 
         protected void set_BAD_FILE_NAME()
         {
